49bdf0e5ef2ce69adf162024097e39f6efda7ebe
Frank Blau <fjblau@gmail.com>
2023-05-27 13:24:14 +0200

    Update aqiToElastic.py

--- aqiToElastic.py
+++ aqiToElastic.py
@@ -4,13 +4,25 @@
 # https://gist.github.com/kadamski/92653913a53baf9dd1a8
 from __future__ import print_function
 import serial, struct, sys, time, json
+from json import encoder    
+
 import aqi
 import pyhue
-import blynklib
 from phue import Bridge
 from decimal import Decimal
 from elasticsearch import Elasticsearch 
 from datetime import datetime
+import paho.mqtt.client as paho
+
+encoder.FLOAT_REPR = lambda o: format(o, '.2f')
+
+def on_connect(client, userdata, flags, rc):
+    print("CONNACK received with code %d." % (rc))
+
+client = paho.Client()
+client.on_connect = on_connect
+client.connect('172.104.235.199')
+
 if sys.version_info[0] == 3:
     from urllib.request import urlopen
 else:
@@ -19,6 +31,10 @@ else:
     # might be around one day
     from urllib import urlopen
     
+import xml.etree.ElementTree as ET 
+import requests
+
+
 DEBUG = 0
 CMD_MODE = 2
 CMD_QUERY_DATA = 4
@@ -51,21 +67,50 @@ def setDeckColor(aqi):
     red = 65386
     purple = 55524
     
+    bridge.set_light('Deck', 'bri', 50, transitiontime=1)
+    
     if aqi < 20:    
         bridge.set_light('Deck', 'on', False)
     elif aqi < 50:  
         bridge.set_light('Deck', 'on', True)  
         light_names["Deck"].hue = green
-    elif aqi < 100:
-        bridge.set_light('Deck', 'on', True)
-        light_names["Deck"].hue = yellow    
     elif aqi < 150:
         bridge.set_light('Deck', 'on', True)
-        light_names["Deck"].hue = orange    
+        light_names["Deck"].hue = yellow    
+    elif aqi < 175:
+        bridge.set_light('Deck', 'on', True)
+        light_names["Deck"].hue = green    
     elif aqi < 200:
         bridge.set_light('Deck', 'on', True)
         light_names["Deck"].hue = red 
-            
+ 
+def setDeckColorSolar(xray):
+    bridge = Bridge('10.0.0.1')
+    light_names = bridge.get_light_objects('name')
+    green = 24267
+    yellow = 10187
+    orange = 6792
+    red = 65386
+    purple = 55524
+    
+    bridge.set_light('Deck', 'bri', 60, transitiontime=1)
+    
+    if xray < 50:    
+        bridge.set_light('Deck', 'on', False)
+        light_names["Deck"].hue = green
+    elif xray < 100:  
+        bridge.set_light('Deck', 'on', True)  
+        light_names["Deck"].hue = green
+    elif xray < 150:  
+        bridge.set_light('Deck', 'on', True)  
+        light_names["Deck"].hue = yellow
+    elif xray < 200:  
+        bridge.set_light('Deck', 'on', True)  
+        light_names["Deck"].hue = orange
+    elif aqi < 300:
+        bridge.set_light('Deck', 'on', True)
+        light_names["Deck"].hue = red    
+                   
 def dump(d, prefix=''):
     print(prefix + ' '.join(x.encode('hex') for x in d))
 
@@ -136,23 +181,50 @@ def cmd_set_id(id):
     ser.write(construct_command(CMD_DEVICE_ID, [0]*10+[id_l, id_h]))
     read_response()
 
+def cToF(f):
+    celsius = round((f - 32) * 5/9,2)
+    return celsius
+
+def getSolar():
+    url = "https://www.hamqsl.com/solarxml.php"
+    data = requests.get(url)
+    root = ET.fromstring(data.content)
+    #for child in root.iter('*'):
+    #    print(child.tag)
+    for tag in root.iter('solarflux'):
+        solarflux=int(tag.text)
+        print("solarflux:", solarflux)
+    return solarflux
 
 if __name__ == "__main__":
+        client.loop_start()
+         
+        xray=getSolar()
         singleEnv = {}
-        url = 'http://api.weatherlink.com/v1/NoaaExt.json?user=001D0A0100EE&pass=2Ellbelt!&apiToken=B1A41C82525B4BB7AB170F5915D7C316'
+        url = 'https://api.ecowitt.net/api/v3/device/real_time?application_key=971DEB582CFA59049B166125BB9DEF1B&api_key=009d974f-d4ae-4d61-ac9d-35306e2e2c67&mac=C4:5B:BE:6D:E3:44&call_back=all'
         data = urlopen(url).read()
+        #print(data)
         d = json.loads(data)
         singleEnv['source'] = "dornbirn"
-        singleEnv['tempf'] = Decimal(d["temp_c"])
-        singleEnv['bar'] = Decimal(d["pressure_mb"])
-        singleEnv['hum'] = Decimal(d["relative_humidity"])
-        singleEnv['wind_dir'] = Decimal(d["wind_degrees"])
+        singleEnv['xray'] = Decimal(xray)
+        singleEnv['tempf'] = Decimal(d["data"]["outdoor"]["temperature"]["value"])
+        singleEnv['tempc'] = cToF(Decimal(d["data"]["outdoor"]["temperature"]["value"]))
+        singleEnv['bar'] = round(Decimal(d["data"]["pressure"]["relative"]["value"])* Decimal(33.8639), 1)
+        singleEnv['hum'] = Decimal(d["data"]["outdoor"]["humidity"]["value"])
+        singleEnv['rain_hourly'] = Decimal(d["data"]["rainfall"]["hourly"]["value"])
+        singleEnv['rain_daily'] = Decimal(d["data"]["rainfall"]["daily"]["value"])
+        singleEnv['rain_rate'] = Decimal(d["data"]["rainfall"]["rain_rate"]["value"])
+        singleEnv['wind_dir'] = Decimal(d["data"]["wind"]["wind_direction"]["value"])
+        singleEnv['wind_gust'] = Decimal(d["data"]["wind"]["wind_gust"]["value"])
+        singleEnv['fridgeT'] = cToF(Decimal(d["data"]["temp_and_humidity_ch2"]["temperature"]["value"]))
+        singleEnv['freezerT'] = cToF(Decimal(d["data"]["temp_and_humidity_ch3"]["temperature"]["value"]))
+	#�singleEnv['soilCh1']= Decimal(d["data"]["soil_ch2"]["soilmoisture"]["value"])
         cmd_set_sleep(0)
         cmd_set_mode(1);
         
         es = Elasticsearch(
                 ['http://172.104.235.199'],
-                port=9200,
+                port=59200,
                 )
         
         for t in range(10):
@@ -164,10 +236,15 @@ if __name__ == "__main__":
                 singleEnv["aqi_10"] = aqi.to_iaqi(aqi.POLLUTANT_PM10, str(values[1]))
                 singleEnv["compositeAQI"] = aqi.to_aqi([(aqi.POLLUTANT_PM25, str(values[0])), (aqi.POLLUTANT_PM10, str(values[1]))])
                 singleEnv['timestamp'] = datetime.now().isoformat()
+                
                 #setDeckColor(singleEnv["compositeAQI"])
+                setDeckColorSolar(xray)
+                
                 if values[0] > 0:
                     es.index(index='sds2', body=singleEnv)
-                    #blynk.virtual_write(7, singleEnv["compositeAQI"])
+                    client.publish("fjblau/env/pm2.5", str(singleEnv["compositeAQI"]) , qos=1)
+                    client.publish("fjblau/env/xray", str(singleEnv["xray"]) , qos=1)
+
                     print(singleEnv)
                 time.sleep(2)
         
